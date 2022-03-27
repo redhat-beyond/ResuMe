aef2b90d5bb28d362bb477d8459ad4be68808053
OmerCS8 <omercs8@gmail.com>
2022-03-27 19:30:42 +0300

    renaming resumeApp to Feed

--- ResuMe/settings.py
+++ ResuMe/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resumeApp.apps.ResumeappConfig',
+    'Feed.apps.FeedConfig',
     'userProfile.apps.UserprofileConfig',
 ]
 
