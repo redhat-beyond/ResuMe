c5618cd16e4c245fa5632759829a5bf70eb29534
Matan Peretz <matanp103@gmail.com>
2022-03-31 18:13:57 +0300

    Matching pep8
    - matching 0004_add_posts_test.py to pep8 standard

--- posts/migrations/0004_add_posts_test.py
+++ posts/migrations/0004_add_posts_test.py
@@ -4,32 +4,23 @@ import random
 
 
 class Migration(migrations.Migration):
-    
     dependencies = [
         ('posts', '0003_rating'),
     ]
 
     def generate_post_test_data(apps, schema_editor):
         from django.contrib.auth.models import User
-        
         users = User.objects.all()
 
-
-
-        resume =  Resume(users[0], "Hi this is my resume file :) " + users[0].first_name),
-
-      
-
-
-
         with transaction.atomic():
-            resume =  Resume(author=users[0], description= "Hi this is my resume file :) " + users[0].first_name, resume_file = "files/Alon_Shakaroffs_resume.pdf" )
+            resume = Resume(author=users[0], description="Hi this is my resume file :) " +
+                            users[0].first_name, resume_file="files/Alon_Shakaroffs_resume.pdf")
             resume.save()
-            r = range(2,5)
+            r = range(2, 5)
             for i in r:
-                rating = Rating(author = users[i], resume=resume, design_rating =random.randrange(1,5), 
-                grammar_rating = random.randrange(1,5), conciseness_rating = random.randrange(1,5), 
-                skill_relevance_rating = random.randrange(1,5))
+                rating = Rating(author=users[i], resume=resume, design_rating=random.randrange(1, 5),
+                                grammar_rating=random.randrange(1, 5), conciseness_rating=random.randrange(1, 5),
+                                skill_relevance_rating=random.randrange(1, 5))
                 rating.save()
 
     operations = [
