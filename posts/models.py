36e0b0adc1174a80556ab9b3cfcbd63fc257d3e6
OmerCS8 <omercs8@gmail.com>
2022-03-31 11:13:56 +0300

    Adding Post model
    Adding parent (non abstract) Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# --------------------------------- Post: parent of Poll And Resume----------------------------------
+
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
