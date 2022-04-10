a76c69b94135edb662c8897393ae2ed17f6d5c9b
Yuli Suliman <94524448+yulisuliman@users.noreply.github.com>
2022-04-10 18:45:46 +0300

    Add profession to users model (#79)
    - Adding profession field to users model.
    - Creating test for profession field.
    - Changing tests name to be more meaningful.

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(default='profile-icon.png', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
+    profession = models.TextField(max_length=100, blank=True, validators=[MaxLengthValidator(100)])
     User._meta.get_field('email')._unique = True
 
     def __str__(self):
