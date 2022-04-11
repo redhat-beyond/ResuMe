e0fe2bb06789d0573d786dd1eec097b23b61b569
omer cohen shor <omercs8@gmail.com>
2022-04-11 21:07:21 +0300

    Adding apps urls and blank templates (#72)
    - Adding urls to all apps.
    - Adding a blank html template page to every feature.
    - Linking urls to templates via views

--- direct_message/views.py
+++ direct_message/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'direct_message/home.html', {'title': 'direct'})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    return render(request, 'users/profile.html', {'title': 'profile'})
+
+
+def login(request):
+    return render(request, 'users/login.html', {'title': 'login'})
+
+
+def logout(request):
+    return render(request, 'users/logout.html', {'title': 'logout'})
