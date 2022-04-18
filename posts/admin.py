7a684c9f0b57ff61ba6ea95416a199ea82d525e1
omer cohen shor <omercs8@gmail.com>
2022-04-18 22:34:31 +0300

    Adding 'PollFile' model (#52)
    - Adding PollFile model that stores files that belong to a Poll.
    - Adding tests and migrations.

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Resume, Rating, Comment, Poll
+from .models import Resume, Rating, Comment, Poll, PollFile
 
 admin.site.register(Resume)
 admin.site.register(Rating)
 admin.site.register(Comment)
 admin.site.register(Poll)
+admin.site.register(PollFile)
