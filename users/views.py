f76376d29bda80092d80d308fa03e63db1158b46
Yuli Suliman <94524448+yulisuliman@users.noreply.github.com>
2022-04-18 18:22:05 +0300

    Create profile views (#92)
    - Create profile.html file
    - allow users to update their settings.
    - allow users add a new profile picture/ bio/ profession.
    - resize this image when it is uploaded.
    - install django-crispy-forms.
    - Add edit-profile page.
    - Add test_views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html', {'title': 'profile'})
 
@@ -11,3 +14,23 @@ def login(request):
 
 def logout(request):
     return render(request, 'users/logout.html', {'title': 'logout'})
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('users-profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'users/edit-profile.html', context)
