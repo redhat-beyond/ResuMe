08fc56c240518185ba5136f9f4583e01e538f8e0
Matan P <36789773+MatanP12@users.noreply.github.com>
2022-04-21 14:03:56 +0300

    Adding Login functionality (#84)
    - Changing paths so to login/logout paths would be `localhost:8000/login` instead
    of `localhost:8000/users/login`
    - Updating the base template accordingly.
    - Creating basic login page form.
    - Adding `@login_required` to feed, direct, profile views
    - Adding tests for login page

--- users/test_views.py
+++
@@ -1,8 +0,0 @@
-class TestViews:
-    def test_profile_view(self, client):
-        response = client.get('/users/profile/')
-        assert response.status_code == 302
-
-    def test_edit_profile_view(self, client):
-        response = client.get('/users/edit-profile/')
-        assert response.status_code == 302
