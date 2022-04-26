a5ed5e30b36f6fe984fefed5ed679b9f7da92e90
yulisuliman <yulisuliman1234@gmail.com>
2022-04-26 22:31:54 +0300

    test

--- direct_message/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def home(request):
-    return render(request, 'direct_message/home.html', {'title': 'direct'})
